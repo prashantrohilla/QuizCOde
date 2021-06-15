cebe6f31be9c19a7ebada6f7f5069b3ad6f90c4f
prashantrohilla <prashantrohilla32@gmail.com>
2021-06-15 15:54:06 +0530

    new

--- app/src/main/java/com/mcqquiz/quizcode/MainActivity.kt
+++ app/src/main/java/com/mcqquiz/quizcode/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import com.airbnb.lottie.LottieAnimationView
 import com.mcqquiz.quizcode.R.id.android
 import com.mcqquiz.quizcode.R.id.startButton
@@ -11,22 +12,25 @@ import com.mcqquiz.quizcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
 
         val scale=AnimationUtils.loadAnimation(this,R.anim.scale)
         val translate=AnimationUtils.loadAnimation(this,R.anim.translate)
 
-        val android=findViewById<LottieAnimationView>(android)
-        val startButton=findViewById<LottieAnimationView>(startButton)
+        binding.android.startAnimation(scale)
+        binding.startButton.startAnimation(translate)
 
-        android.startAnimation(scale)
-        startButton.startAnimation(translate)
+        val toast = Toast.makeText(applicationContext, "Developed BY Prashant Rohilla", Toast.LENGTH_LONG)
+        toast.show()
 
-        startButton.setOnClickListener{
+        binding.startButton.setOnClickListener{
             val intent = Intent(this,SelectQuiz::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/mcqquiz/quizcode/SelectQuiz.kt
+++ app/src/main/java/com/mcqquiz/quizcode/SelectQuiz.kt
@@ -1,10 +1,22 @@
 package com.mcqquiz.quizcode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
+import com.mcqquiz.quizcode.databinding.ActivitySelectQuizBinding
 
 class SelectQuiz : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_quiz)
+        supportActionBar?.hide()
+
+        
     }
 }
